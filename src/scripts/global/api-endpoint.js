c2146ab7fb68df698300629320b787351065e04f
NafiAdiansyah04 <nafiadiansyah04@gmail.com>
2024-06-25 19:12:44 +0700

    Update api-endpoint.js

--- src/scripts/global/api-endpoint.js
+++ src/scripts/global/api-endpoint.js
@@ -1,4 +1,4 @@
-const CONFIG = { BASE_URL: 'https://backend-ww-six.vercel.app' };
+const CONFIG = { BASE_URL: 'https://coba-backend-1.vercel.app/' };
 
 const API_ENDPOINT = {
   ARTICLES: `${CONFIG.BASE_URL}/articles`,
