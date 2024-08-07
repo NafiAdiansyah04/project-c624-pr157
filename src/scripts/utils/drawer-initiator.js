11ffeff66192c03ef310678eda1b58b1e7f95055
NafiAdiansyah04 <nafiadiansyah04@gmail.com>
2024-08-07 20:32:22 +0700

    Update drawer-initiator.js

--- src/scripts/utils/drawer-initiator.js
+++ src/scripts/utils/drawer-initiator.js
@@ -1,30 +1,20 @@
 const DrawerInitiator = {
   init({ button, drawer, content }) {
     button.addEventListener('click', (event) => {
-      this._toggleDrawer({ event, drawer });
+      this._toggleDrawer({ event, button, drawer });
     });
 
     content.addEventListener('click', (event) => {
-      this._closeDrawer({ event, drawer });
-    });
-
-    // Tambahkan event listener untuk menutup drawer ketika item di dalam drawer diklik
-    const drawerItems = drawer.querySelectorAll('.nav-link');
-    drawerItems.forEach((item) => {
-      item.addEventListener('click', (event) => {
-        this._closeDrawer({ event, drawer });
-      });
+      this._closeDrawer({ event, button, drawer });
     });
   },
 
-  _toggleDrawer({ event, drawer }) {
+  _toggleDrawer({ event }) {
     event.stopPropagation();
-    drawer.classList.toggle('open'); // Mengganti kelas CSS untuk membuka/menutup drawer
   },
 
-  _closeDrawer({ event, drawer }) {
+  _closeDrawer({ event }) {
     event.stopPropagation();
-    drawer.classList.remove('open'); // Menghapus kelas CSS untuk menutup drawer
   },
 };
 
